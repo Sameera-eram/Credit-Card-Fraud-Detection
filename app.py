2b23d512753dd294f000726bc21cebf6b7b03ee4
Sameera-eram <153179786+Sameera-eram@users.noreply.github.com>
2024-09-29 20:44:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv("creditcard.csv")
+
+legit = data[data.Class==0]
+fraud = data[data.Class==1]
+
+legit_sample = legit.sample(n = len(fraud),random_state=2)
+data = pd.concat([legit_sample,fraud],axis=0)
+
+X = data.drop('Class', axis =1)
+y = data['Class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, stratify=y, random_state=2)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+train_acc = accuracy_score(model.predict(X_train), y_train)
+test_acc = accuracy_score(model.predict(X_test), y_test)
+
+st.title('Credit Card Fraud Detection Model')
+input_df = st.text_input("Enter All Required Features Values")
+input_df_splited = input_df.split(',')
+
+submit = st.button("Submit")
+
+if submit:
+    features = np.asarray(input_df_splited,dtype=np.float64)
+    prediction = model.predict(features.reshape(1,-1))
+
+    if prediction[0] == 0:
+        st.write("Legitimate Transaction")
+    else:
+        st.write("Fradulant Transaction")
